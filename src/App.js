8369ad6b52e675326a9dd7852a0972975ec1926a
Po-Ting Lu <amos040425@gmail.com>
2024-12-01 16:39:34 +0800

    Added event search function

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Headers from './components/Header.tsx'
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Headers from './components/Header.tsx';
 import Home from './pages/Home.tsx';
 import Signup from './pages/auth/signup/Page.tsx';
 import Signin from './pages/auth/signin/Page.tsx';
 import SeatSelection from './pages/ticket/[activity_id]/SeatSelection.tsx';
 import Order from './pages/order/Order.tsx';
+import SearchResults from './pages/search/SearchResults.tsx'
+
+// 定義 RootLayout，包含 Header
+const RootLayout = () => {
+  return (
+    <div>
+      <Headers />
+      <Outlet /> {/* 用於渲染子路由 */}
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <RootLayout />, // 根布局
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/signup", element: <Signup /> },
+      { path: "/signin", element: <Signin /> },
+      { path: "/SeatSelection/:activity_id", element: <SeatSelection /> },
+      { path: "/Order", element: <Order /> },,
+      { path: "/:query", element: <SearchResults /> },
+    ],
   },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/signin",
-    element: <Signin />,
-  },
-  {
-    path: "/SeatSelection/:activity_id", // 將動態參數改為 :activity_id
-    element: <SeatSelection />,
-  },
-  {
-    path: "/Order",
-    element: <Order />,
-  }
 ]);
 
 function App() {
   return (
     <div className="w-screen h-screen">
-      <Headers></Headers>
       <RouterProvider router={router} />
     </div>
   );
